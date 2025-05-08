9d7e42e76a1a76ca5f4484b3e997ee9e7f4bf203
Danilo13-Production <storozukd30@gmail.com>
2025-05-08 16:30:49 +0300

    да

--- mein.py
+++ mein.py
@@ -1,3 +1,7 @@
 name = input(" ім'я: ")
-age = input(" вік: ")
+age = int(input(" вік: "))
 print(f"Привіт {name}, тобі {age}!")
+if age > 18:
+    print("Вхід дозволено!")
+else:
+    print("Вхід заборонено!")
