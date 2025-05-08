752f12658a4c941517d2fc83b4fe5a2e3b450f03
Danilo13-Production <storozukd30@gmail.com>
2025-05-08 16:31:00 +0300

    да

--- mein.py
+++ mein.py
@@ -5,3 +5,21 @@ if age > 18:
     print("Вхід дозволено!")
 else:
     print("Вхід заборонено!")
+import random
+
+    secret_number = random.randint(1, 10)
+    attempts = 3
+
+    while attempts > 0:
+        guess = int(input("Вгадай число від 1 до 10: "))
+        if guess == secret_number:
+            print("Вітаю! Ви вгадали!")
+            break
+        elif guess > secret_number:
+            print("Менше")
+        else:
+            print("Більше")
+        attempts -= 1
+
+    if attempts == 0:
+        print(f"Ви програли. Загадане число було {secret_number}")
